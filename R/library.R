e66f24ef3dfac504616ec88608a46dae3481df39
Egor Gazarov <Egor Gazarov>
2017-01-14 00:09:57 +0100

    EG-0114-0010

--- R/library.R
+++ R/library.R
@@ -1,2 +1,17 @@
-# this file will load libraries used in the project
-# to be later sourced in main project file
+# Loading all required packages
+# If loading failes - installing these packages
+
+foo <- function(x){
+  for( i in x ){
+    #  require returns TRUE invisibly if it was able to load package
+    if( ! require( i , character.only = TRUE ) ){
+      #  If package was not able to be loaded then re-install
+      install.packages( i , dependencies = TRUE )
+      #  Load package after installing
+      require( i , character.only = TRUE )
+    }
+  }
+}
+
+#  Then try/install packages...
+foo( c("googleVis" , "reshape2") )
